55ca27379e68b91dcf4ad2b5690c42a4f03ecc54
nureex <nureexamahmud@gmail.com>
2023-09-25 17:13:31 -0700

    Print Lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list int - Prints elements of a singly linked lists.
+ * Return: Integer.
+ **/
+
+size_t print_listint(const listint_t *k)
+{
+	const listint_t *tp;
+	unsigned int cntr = 0;
+
+	tp = k;
+	while (tp)
+	{
+		printf("%d\n", tp->n);
+		cntr++;
+		tp = tp->next;
+	}
+	return (cntr);
+}
